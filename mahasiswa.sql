a02c623c2a39d2adf2c791e448053065bc3ca46d
Irma-Nurkhofifah <61815097+Irma-Nurkhofifah@users.noreply.github.com>
2020-06-12 20:35:42 +0800

    Add files via upload

---
+++ mahasiswa.sql
@@ -0,0 +1,9 @@
+create database mahasiswa;
+use mahasiswa;
+create table table_mahasiswa(
+nomhs bigint primary key,
+nama varchar(30),
+jurusan varchar(15),
+prodi varchar(20));
+
+desc tabel_mahasiswa;
